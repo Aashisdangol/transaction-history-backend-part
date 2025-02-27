e69d9129641dc8e44b600a3a3810d3c62a72e173
Aashis <12200199@students.koi.edu.au>
2025-02-27 22:48:19 +1100

    initialize the PORT and express app

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 // Create express app
 import express from 'express'
 
+import path from 'path'
+import fs from 'fs'
+
+// initialize an express app 
+const app = express()
+
+// defining the port 
+const PORT = 300
+
+app.get('/', (req, res) => {
+    console.log("Req", req)
+    //send a html home page code as a response
+    res.send("<div><a href='/signup'>Sign Up</a> <a href='/login'>Login</a><hr /><h1 style='color: red'>Node JS Login System</h1></div>")
+})
+
+
+
 //Run Server
 //COnnect with DB
 //Create USer route
